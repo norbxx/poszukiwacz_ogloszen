6e62f62ec15dd5e8c8e61cf61e88535775a2d365
Norbert <norbx@MacBook-Pro-Norbert.local>
2023-07-24 20:24:51 +0200

    Initial commit

---
+++ src/main/java/com/example/poszukiwacz_ogloszen/PoszukiwaczOgloszenApplication.java
@@ -0,0 +1,13 @@
+package com.example.poszukiwacz_ogloszen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoszukiwaczOgloszenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoszukiwaczOgloszenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/poszukiwacz_ogloszen/PoszukiwaczOgloszenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poszukiwacz_ogloszen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoszukiwaczOgloszenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
